c9df4304cadb11af80d9a61db1900b6f5e95251a
Andreas Neofytou <github@neofytou.com>
2023-07-08 12:13:27 +0300

    mm

--- lib/components/my_app_bar.dart
+++ lib/components/my_app_bar.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:provider/provider.dart';
 
 AppBar myAppBar(BuildContext context, {String? title}) {
-  User? user = context.select((value) => null);
   return AppBar(
       systemOverlayStyle:
           MediaQuery.of(context).platformBrightness == Brightness.light
